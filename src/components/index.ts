7e38f3e79270144756da21c03bfc7fc831748407
AdelhamA <adell97.work@gmail.com>
2022-08-22 19:53:33 +0200

    export the components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Text } from "./Text/Text";
+export { Card } from "./card/card";
+export { Chip } from "./chip/chip";
+export { Button } from "./button/button";
+export { PlusMinus } from "./plus-minus/plus-minus";
+export { CartCard } from "./cart-card/cart-card";
+export { Empty } from "./empty/empty";
