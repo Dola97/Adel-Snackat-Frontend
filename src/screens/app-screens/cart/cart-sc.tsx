588b47deda2ad0e0d3bb8de1932bcc54370f6baa
AdelhamA <adell97.work@gmail.com>
2022-08-22 19:55:58 +0200

    cart screen

--- src/screens/app-screens/cart/cart-sc.tsx
+++ src/screens/app-screens/cart/cart-sc.tsx
@@ -1,5 +1,40 @@
 import React from "react";
-
+import { SafeAreaView, ScrollView } from "react-native";
+import { palette } from "theme";
+import { CartCard, Empty, Text } from "components";
+import { selectCart } from "src/store/cart-slice";
+import { useSelector } from "react-redux";
 export const CartScreen = () => {
-  return <></>;
+  console.log("dddjksfkjdkl");
+  const cart = useSelector(selectCart);
+  if (cart.length === 0) {
+    return (
+      <Empty
+        children={
+          <Text
+            children="Your Cart Is Empty"
+            type={"bold"}
+            isTouchable={false}
+            color={palette.black}
+            fz={24}
+          />
+        }
+      />
+    );
+  }
+  return (
+    <SafeAreaView style={{ backgroundColor: palette.white, flex: 1 }}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+        {cart.map((item, key) => (
+          <React.Fragment key={`line_items_keys_${key}`}>
+            <CartCard
+              id={item.id}
+              quantity={item.quantity}
+              image={item.image}
+            />
+          </React.Fragment>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
