82928794f9dee9c136cd613eec3f2e7a98affa01
AdelhamA <adell97.work@gmail.com>
2022-08-22 19:53:44 +0200

    button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleProp, TouchableOpacity, ViewStyle } from "react-native";
+
+interface Props {
+  styleButton?: StyleProp<ViewStyle>;
+  children: any;
+  loading: boolean | undefined;
+  loadingComponent?: any;
+  onPress: () => void;
+}
+export const Button: React.FC<Props> = (props) => {
+  return (
+    <TouchableOpacity
+      disabled={props.loading}
+      onPress={props.onPress}
+      style={props.styleButton}
+    >
+      {props.loading ? props.loadingComponent : props.children}
+    </TouchableOpacity>
+  );
+};
