29ea19e727adf5fb649da88d259708ff327ddf67
AdelhamA <adell97.work@gmail.com>
2022-08-22 19:56:06 +0200

    products screen

--- src/screens/app-screens/products/products-sc.tsx
+++ src/screens/app-screens/products/products-sc.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { SafeAreaView, TouchableOpacity } from "react-native";
+import { palette } from "theme";
+import { ProductsList } from "./components/list";
+import useFetch from "hooks/useFetch";
 
-export const ProductsScreen = () => {
-  return <></>;
+import { Tess } from "./components/text";
+import { Cart } from "assets/svgs";
+import { useNavigation } from "@react-navigation/native";
+import { Empty, Text } from "components";
+export const ProductsScreen = ({ navigation }: any) => {
+  const { data, loading, status, error } = useFetch("/products");
+  const { navigate } = useNavigation();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigate("cart")}>
+          <Cart />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+  if (error) {
+    console.warn("error");
+  }
+
+  return (
+    <SafeAreaView style={{ backgroundColor: palette.white, flex: 1 }}>
+      <ProductsList
+        data={data}
+        onRefresh={() => console.log("refresh")}
+        endReached={() => console.log("refresh")}
+        refreshing={loading}
+      />
+      <Tess />
+    </SafeAreaView>
+  );
 };
